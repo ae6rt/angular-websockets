554c51fcfcc3c22aee84dba3762f5856bf21fb1f
Mark Petrovic <mspetrovic@gmail.com>
2013-12-08 14:45:27 -0800

    hone the scope.apply() to update the model

--- static/js/app.js
+++ static/js/app.js
@@ -2,27 +2,21 @@ var app = angular.module('app', []);
 
 app.controller('controller', function ($scope, websocketService) {
     $scope.msg = "init";
-
-    $scope.update = function (m) {
-        $scope.msg = m;
-        $scope.$apply();  // update model, driven from outside Angular event cycle
-    };
-
-    websocketService.start("ws://localhost:8080/events", $scope.update);
-
+    websocketService.start("ws://localhost:8080/events", $scope);
 });
 
 app.factory('websocketService', function () {
         return {
-            start: function (url, callback) {
+            start: function (url, scope) {
                 var websocket = new WebSocket(url);
                 websocket.onopen = function () {
                 };
                 websocket.onclose = function () {
                 };
                 websocket.onmessage = function (evt) {
-                    var msg = JSON.parse(evt.data);
-                    callback(msg.message);
+                    scope.$apply(function () {
+                        scope.msg = JSON.parse(evt.data).message;
+                    });
                 };
             }
         }
