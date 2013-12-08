d078b8bcdffd6a9c52f773e839f7d8692e2dd6a4
Mark Petrovic <mspetrovic@gmail.com>
2013-12-08 06:52:14 -0800

    update readem

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Clone and start the server:
 
 ./gradlew clean run
 
-Then point your browser to http://localhost:8080/index.html and
-see the time updated in the view every 5 seconds.
+Then point your browser to http://localhost:8080/index.html, open
+ the browser debugging console, and see the date updated on the console
+ and hopefully in the view.
+
--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,17 @@
 var app = angular.module('app', []);
 
-app.controller('controller', function ($scope, $timeout) {
+app.controller('controller', function ($scope) {
     $scope.msg = "init";
 
-    $scope.setMessage = function (msg) {
-        $scope.msg = msg;
-    };
-
     var ws = new WebSocket("ws://localhost:8080/events");
+
     ws.onopen = function () {
+    };
+    ws.onclose = function () {
     };
     ws.onmessage = function (evt) {
         var msg = JSON.parse(evt.data);
+        $scope.msg = msg.message;
         console.log(msg.message);
-        $scope.setMessage(msg.message);
-    };
-    ws.onclose = function () {
-        console.log("Connection is closed...");
     };
 });
