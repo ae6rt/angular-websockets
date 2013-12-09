82e9e3a6cefac6acc85366736eef6418fff89bbd
Mark Petrovic <mspetrovic@gmail.com>
2013-12-08 16:42:55 -0800

    remove controller specific state from websocket callback

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,26 @@
 var app = angular.module('app', []);
 
 app.controller('controller', function ($scope, websocketService) {
-    $scope.msg = "init";
-    websocketService.start("ws://localhost:8080/events", $scope);
+    $scope.msg = "...";
+
+    websocketService.start("ws://localhost:8080/events", function (evt) {
+        var obj = JSON.parse(evt.data);
+        $scope.$apply(function () {
+            $scope.msg = obj.message
+        });
+    });
 });
 
 app.factory('websocketService', function () {
         return {
-            start: function (url, scope) {
+            start: function (url, callback) {
                 var websocket = new WebSocket(url);
                 websocket.onopen = function () {
                 };
                 websocket.onclose = function () {
                 };
                 websocket.onmessage = function (evt) {
-                    scope.$apply(function () {
-                        scope.msg = JSON.parse(evt.data).message;
-                    });
+                    callback(evt);
                 };
             }
         }
