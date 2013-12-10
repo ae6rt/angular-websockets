23a3b190b1c327478564d5d7eef502915e2f6052
Mark Petrovic <mspetrovic@gmail.com>
2013-12-10 09:49:58 -0800

    separate services from core app

--- static/js/app.js
+++ static/js/app.js
@@ -1,33 +1,15 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['services'])
+    .run(function (websockets) {
+        websockets.start("ws://localhost:8080/events");
+    });
 
-app.controller('controller', function ($scope,
-                                       websocketService) {
+app.controller('controller', function ($scope, websockets) {
     $scope.msg = "...";
 
-    websocketService.start("ws://localhost:8080/events", function (evt) {
+    $scope.myUuid = websockets.addListener(function (evt) {
         var obj = JSON.parse(evt.data);
         $scope.$apply(function () {
             $scope.msg = obj.message
         });
     });
 });
-
-app.factory('websocketService', function () {
-        var websocket;
-        return {
-            start: function (url, callback) {
-                websocket = new WebSocket(url);
-                websocket.onopen = function () {
-                };
-                websocket.onclose = function () {
-                };
-                websocket.onmessage = function (evt) {
-                    callback(evt);
-                };
-            },
-            stop: function () {
-                websocket.close();
-            }
-        }
-    }
-);
