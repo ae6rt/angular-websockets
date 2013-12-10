2b41b899683dc650cd67366b2196d25d9323a50c
Mark Petrovic <mspetrovic@gmail.com>
2013-12-10 07:50:16 -0800

    add RFC4122 UUID creation to id listeners for add/remove

--- static/js/app1.js
+++ static/js/app1.js
@@ -1,5 +1,24 @@
-var services = angular.module('services', []);
+var uuids = angular.module('uuids', []);
+uuids.factory("rfc4122", function () {
+    return {
+        newuuid: function () {
+            // http://www.ietf.org/rfc/rfc4122.txt
+            var s = [];
+            var hexDigits = "0123456789abcdef";
+            for (var i = 0; i < 36; i++) {
+                s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+            }
+            s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+            s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+            s[8] = s[13] = s[18] = s[23] = "-";
 
+            var uuid = s.join("");
+            return uuid;
+        }
+    }
+});
+
+var services = angular.module('services', ['uuids']);
 services.factory("t", function () {
     var someVar = "foo";
     return {
@@ -8,7 +27,6 @@ services.factory("t", function () {
         }
     }
 });
-
 services.factory("u", function () {
     var someVar = "bar";
     return {
@@ -17,8 +35,7 @@ services.factory("u", function () {
         }
     }
 });
-
-services.factory("websockets", function () {
+services.factory("websockets", function (rfc4122) {
     var listeners = [];
     var socket = null;
     return {
@@ -38,7 +55,9 @@ services.factory("websockets", function () {
             socket.close();
         },
         addListener: function (t) {
+            var uuid = rfc4122.newuuid();
             listeners.push(t);
+            console.log("added listener " + uuid);
         }
     }
 });
